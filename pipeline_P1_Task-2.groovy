4b4bca6e337b2be69906a2a98647f88cc18b57f3
Ayaan khan <114697694+Ayaanullakhan@users.noreply.github.com>
2025-05-23 01:40:15 +1000

    Update pipeline_P1_Task-2.groovy

--- pipeline_P1_Task-2.groovy
+++ pipeline_P1_Task-2.groovy
@@ -1,5 +1,8 @@
 pipeline {
     agent any
+    environment{
+        RECIPIENTS = 'ayaanullakhan11@gmail.com'
+    }
 
     stages {
         stage('Checkout') {
@@ -16,9 +19,28 @@ pipeline {
 
         stage('Run Tests') {
             steps {
-                sh 'npm test || true' // Allow pipeline to continue even if tests fail
+                script{
+                    try{
+                        sh 'npm test || tee test.log' 
+                        currentBuild.description = 'Tests Passed'
+                    } catch (err) {
+                        currentBuild.result = 'FAILURE'
+                        currentBuild.description = 'Tests Failed'
+                        throw err
+                    }
+                }
+            }
+        post {
+            always{
+                emailext(
+                    subject: "Run Tests - ${currentBuild.currentResult}",
+                    body: """<p>The <b>Run Tests</b> stage has completed with status: <b>${currentBuild.currentResult}</b></p>""",
+                    to: "${env.RECIPIENTS}",
+                    attachmentsPattern: 'test.log'
+                    )
             }
         }
+    }
 
         stage('Generate Coverage Report') {
             steps {
@@ -27,8 +49,27 @@ pipeline {
         }
 
         stage('NPM Audit (Security Scan)') {
-            steps {
-                sh 'npm audit || true'
+             steps {
+                script {
+                    try {
+                        sh 'npm audit | tee audit.log'
+                        currentBuild.description += " | Security Scan Passed"
+                    } catch (err) {
+                        currentBuild.result = 'FAILURE'
+                        currentBuild.description += " | Security Scan Failed"
+                        throw err
+                    }
+                }
+            }
+            post {
+                always {
+                    emailext (
+                        subject: "Security Scan - ${currentBuild.currentResult}",
+                        body: """<p>The <b>Security Scan</b> stage has completed with status: <b>${currentBuild.currentResult}</b></p>""",
+                        to: "${env.RECIPIENTS}",
+                        attachmentsPattern: 'audit.log'
+                    )
+                }
             }
         }
     }
